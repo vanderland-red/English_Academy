734fb2b769e4fdf9c6f0bf9766b11b28c36a158f
Mohammadreza <mohammad.za9297@gmail.com>
2026-08-28 20:19:07 +0330

    Courses detail added in home page

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
+# ساخت پنل ویرایش کاربر در پنل ادمین
 
 class UserAdmin(BaseUserAdmin):
 
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-
-
-def home(request):
-    return render(request, "home.html")
+from apps.courses.models import Course
 
 def about(requset):
     return render(requset, "about.html")
+
+# نمایش پکیج ها در صفحه اصلی
+def home(request):
+    courses = Course.objects.filter(is_active=True)
+
+    return render(request, 'home.html', {'courses': courses}) # ارسال اطلاعات به صفحه اصلی
 
