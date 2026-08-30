5a0625bc4e77f12af8fb4d7532c4bb034e9175e5
Mohammadreza <mohammad.za9297@gmail.com>
2026-08-30 22:33:38 +0330

    Payment and Buy Course is added

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = 'course'
 urlpatterns = [
     path('detail/<int:course_id>/', views.CourseDetailView.as_view(), name='detail'),
+    path('buy/<int:course_id>/', views.BuyCourseView.as_view(), name='buy'),
+    path('payment/<int:enrollment_id>/', views.PaymentView.as_view(), name='payment'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,65 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from .models import Course
-from .models import Enrollment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
+from .models import Course, Enrollment
 
 
+#=================
+# Choice Course
+#=================
 class CourseDetailView(View):
 
     def get(self, request, course_id):
 
-        course = Course.objects.get(
-            id=course_id,
-            is_active=True
+        course = get_object_or_404(Course, id=course_id, is_active=True)
+
+        return render(request, 'course/course.html', {'course': course})
+
+
+
+#=================
+# Buy Course
+#=================
+class BuyCourseView(LoginRequiredMixin, View):
+
+    def get(self, request, course_id):
+
+        course = get_object_or_404(Course,id=course_id, is_active=True)
+
+        # اگر کاربر قبلاً این دوره را خریده باشد
+        enrollment = Enrollment.objects.filter(user=request.user, course=course).first()
+
+        if enrollment and enrollment.is_paid:
+            messages.info(request,'شما قبلاً این دوره را خریداری کرده‌اید.')
+            return redirect('user:dashboard')
+
+        # اگر دوره رو نداره پس براش بساز
+        if not enrollment:
+            enrollment = Enrollment.objects.create(
+                user=request.user,
+                course=course,
+                is_paid=False
+            )
+
+
+        # ارسال به درگاه پرداخت
+        return redirect('course:payment', enrollment.id)
+
+
+#============
+# Payment
+#============
+class PaymentView(LoginRequiredMixin, View):
+
+    def get(self, request, enrollment_id):
+
+        enrollment = get_object_or_404(
+            Enrollment,
+            id=enrollment_id,
+            user=request.user,
+            is_paid=False
         )
-        
 
-        return render(request, "course.html", {'course': course})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class BuyCourseView(LoginRequiredMixin, View):
-
-#     def get(self, request, course_id):
-
-#         course = Course.objects.get(
-#             id=course_id,
-#             is_active=True
-#         )
-
-#         enrollment = Enrollment.objects.create(
-#             user=request.user,
-#             course=course,
-#             is_paid=False
-#         )
-
-#         # اینجا باید کاربر را به درگاه پرداخت بفرستی
+        return render(request, 'course/payment.html',
+            {
+                'enrollment': enrollment,
+                'course': enrollment.course
+            }
+        )
